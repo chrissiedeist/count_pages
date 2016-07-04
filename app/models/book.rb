d49c4628f83f8fbe9c1b52230502a9adb4f9f449
Chrissie Deist <chrissie.deist@gmail.com>
2016-07-03 18:32:46 -0700

    Add validations to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
   has_many :read_books
   has_many :users, :through => :read_books
+
+  validates :num_pages, :title, :presence => true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without num_pages" do
+      book = Book.new(
+        :title => "Book title",
+      )
+      expect(book).to be_invalid
+    end
+
+    it "is invalid without a title" do
+      book = Book.new(
+        :num_pages => 100
+      )
+      expect(book).to be_invalid
+    end
+  end
 end
